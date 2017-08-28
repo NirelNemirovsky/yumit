e4ae5b0006e5062a0e64e563f433b4b23dca4019
ofekochayon <ofek678@gmail.com>
2017-08-29 00:42:25 +0300

    updated DBFunctions

--- BE/DBFunctions.js
+++ BE/DBFunctions.js
@@ -9,29 +9,42 @@ const Schema = mongoose.Schema;
 
 const ShipSchema = new Schema({
   IMO: { type : String, default : '' },
-  MMSI: { type : String, default : '' },
+  name: { type : String, default : '' },
   type: { type : Number },
   year: { type : Number },
-  sizes: [{ Height: { type: Number },
-    Width: { type: Number },
-    Weight: { type: Number }
-  }],
+  sizes: { type: String },
+  gt:    {type : Number},
+  country: { type: String},
 });
 
+/* json
+{
+"imo": "9279252",
+"name": "AMATZIA II",
+"type": "Tug",
+"dest": "HAIFA PORT TUG",
+"etastamp": "Feb 07, 06:28",
+"ship_speed": 2,
+"ship_course": 174.6,
+"timestamp": "Aug 28, 2017 18:19 UTC",
+"year": 2003,
+"gt": 363,
+"sizes": "30 x 11 m",
+"country": "Israel (ISR)"
+}
+*/
+
 const CourseSchema = new Schema({
-   shipIMO:  { type : String, default : ''},
-   SUF: { type : Number },
-   DFS: { type : Number },
-   exitPort: { type : String, default : '' },
-   exitTime: { type : Date, default : Date.now },
-   arrivalPort: { type : String, default : '' },
-   arrivalTime: { type : Date, default : Date.now },
-   sailCourse: { type : Number },
-   sailSpeed: { type : Number }
- });
+  shipIMO:  { type : String, default : ''},
+  dest: { type : String, default : '' },
+  timestamp: { type : Date, default : Date.now },
+  etastamp: { type : Date, default : Date.now },
+  ship_course: { type : Number },
+  ship_speed: { type : Number }
+});
 
 const CellsSchema = new Schema({
-	owner: { type: String }
+  owner: { type: String }
 })
 
 var shipModel = mongoose.model("shipModel", ShipSchema);
@@ -55,7 +68,7 @@ shipSchema.pre('save', function(next) {
 
   // if created_at doesn't exist, add to that field
   if (!this.created_at)
-    this.created_at = currentDate;
+  this.created_at = currentDate;
 
   next();
 });
@@ -69,7 +82,7 @@ courseSchema.pre('save', function(next) {
 
   // if created_at doesn't exist, add to that field
   if (!this.created_at)
-    this.created_at = currentDate;
+  this.created_at = currentDate;
 
   next();
 });
@@ -86,46 +99,44 @@ function close() {
 
 //get ship by IMO
 function getShip(IMO) {
-	shipModel.findOne( { IMO: IMO } );
+  shipModel.findOne( { IMO: IMO } );
 }
 
 //get course by shipIMO
 function getCourses(shipIMO) {
-	courseModel.all( { shipIMO: shipIMO } );
+  courseModel.all( { shipIMO: shipIMO } );
 }
 
 // save the ship
-shipModels.save(function(err) {
-  if (err) throw err;
-
-  console.log('User created!');
-});
-
-// save the course
-courseModels.save(function(err) {
-  if (err) throw err;
-
-  console.log('Course created!');
-});
+function insert(ship) {
+  var shipObj = JSON.parse(ship);
+  var ship = new shipModel({ IMO : shipObj.imo, name : shipObj.name, type : shipObj.type,
+    year : shipObj.year, sizes : shipObj.sizes, gt : shipObj.gt, country : shipObj.country});
+  ship.save(function(err) {
+    if (err) throw err;
+  });
+  var course = new courseModel( { shipIMO : shipObj.imo, dest : shipObj.dest,
+    timestamp :shipObj.timestamp, etastamp : shipObj.etastamp, ship_course : shipObj.ship_course,
+    ship_speed : shipObj.ship_speed });
+    course.save(function(err) {
+      if (err) throw err;
+    });
+};
 
 function coursesBetween(IMO, date1, date2) {
-
-Ship.find({ IMO: IMO }).find(createdAt: {
+  Ship.find({ IMO: IMO }).find(createdAt: {
     $gte: date1,
     $lt: date2
   }).exec(function(err, users) {
-  if (err) throw err;
-
-  console.log(users);
-});
+    if (err) throw err;
+  });
 }
 
 ship.findOneAndRemove({ IMO: IMO }, function(err) {
   if (err) throw err;
   course.findAndRemove({ shipIMO: shipIMO }, function(err) {
-  if (err) throw err;
-});
+    if (err) throw err;
+  });
   // we have deleted the ship
   console.log('Ship deleted!');
 });
-
