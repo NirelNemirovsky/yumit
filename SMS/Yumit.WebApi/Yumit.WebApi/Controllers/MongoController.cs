8b29094c4a70d961c80ef98ed3a46d56e4ba6223
Eran Gil <eran.gil2@gmail.com>
2017-08-29 09:26:19 +0300

    Created a mongo controller to insert to mongo

---
+++ SMS/Yumit.WebApi/Yumit.WebApi/Controllers/MongoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using MongoDB.Driver;
+
+namespace Yumit.WebApi.Controllers
+{
+    public class MongoController : ApiController
+    {
+        private static readonly MongoClient Client;
+        private static readonly IMongoDatabase Db;
+        private const string ConnectionString = "mongodb://Yumit:yumit@ds161793.mlab.com:61793/yumit";
+
+        static MongoController()
+        {
+            Client = new MongoClient(ConnectionString);
+            Db = Client.GetDatabase("yumit");
+        }
+
+        [HttpPost]
+        public IHttpActionResult InsertDocument([FromBody] MongoInsertRequest request)
+        {
+            var collection = Db.GetCollection<object>(request.CollectionName);
+
+            collection.BulkWrite(
+            new List<WriteModel<object>> { new InsertOneModel<object>(request.Document) });
+            Console.WriteLine();
+            return Ok();
+        }
+    }
+
+}
---
+++ SMS/Yumit.WebApi/Yumit.WebApi/MongoInsertRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Yumit.WebApi
+{
+    public class MongoInsertRequest
+    {
+        public string CollectionName { get; set; }
+
+        public object Document { get; set; }
+    }
+}
